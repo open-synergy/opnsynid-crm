3c12307ef68d5d4e291f12ec8d88de42200c8f75
Andhitia Rama <andhitia.r@gmail.com>
2017-05-25 17:18:03 +0700

    [8.0.1.0.0][crm_lead_promotion]

---
+++ crm_lead_promotion/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Promotion for Lead/Opportunity",
+    "version": "8.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_promotion_operation",
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+    ],
+}
