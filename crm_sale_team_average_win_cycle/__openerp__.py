7803fc2e45abe5159a625047ee15383162031eee
Andhitia Rama <andhitia.r@gmail.com>
2018-03-18 22:46:01 +0700

    [8.0.1.0.0] crm_sale_team_average_win_cycle

---
+++ crm_sale_team_average_win_cycle/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Team Average Win Cycle",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "data/ir_cron_data.xml",
+        "views/crm_case_section_views.xml",
+        "views/crm_lead_views.xml",
+    ],
+}
---
+++ crm_sale_team_average_win_cycle/models/crm_case_section.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class CrmCaseSection(models.Model):
+    _inherit = "crm.case.section"
+
+    @api.multi
+    def _compute_average_win_cycle(self):
+        obj_lead = self.env["crm.lead"]
+        for team in self:
+            team.average_win_cycle = 0.0
+            criteria = [
+                ("type", "=", "opportunity"),
+                ("stage_id.probability", "=", 100.00),
+                ("stage_id.on_change", "=", True),
+                ("section_id", "=", team.id),
+            ]
+            opportunities = obj_lead.search(criteria)
+            if len(opportunities) > 0:
+                day_close = opportunities.mapped("day_close")
+                team.average_win_cycle = sum(
+                    day_close) / float(len(day_close))
+
+    average_win_cycle = fields.Float(
+        string="Average Win Cycle",
+        compute="_compute_average_win_cycle",
+    )
