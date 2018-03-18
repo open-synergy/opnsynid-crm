609be708fcd9d1b5ad3be2da14597e01c4f509f2
Andhitia Rama <andhitia.r@gmail.com>
2018-03-18 18:32:50 +0700

    [8.0.1.0.0] crm_sale_team_average_deal_size_won

---
+++ crm_sale_team_average_deal_size_won/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Team Average Deal Size Won",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "views/crm_case_section_views.xml",
+        "views/crm_lead_views.xml",
+    ],
+}
---
+++ crm_sale_team_average_deal_size_won/models/crm_case_section.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class CrmCaseSection(models.Model):
+    _inherit = "crm.case.section"
+
+    @api.multi
+    def _compute_average_deal(self):
+        obj_lead = self.env["crm.lead"]
+        for team in self:
+            team.average_deal_size_won = 0.0
+            criteria = [
+                ("type", "=", "opportunity"),
+                ("stage_id.probability", "=", 100.00),
+                ("stage_id.on_change", "=", True),
+                ("section_id", "=", team.id),
+            ]
+            opportunities = obj_lead.search(criteria)
+            if len(opportunities) > 0:
+                planned_revenues = opportunities.mapped("planned_revenue")
+                team.average_deal_size_won = sum(
+                    planned_revenues) / float(len(planned_revenues))
+
+    average_deal_size_won = fields.Float(
+        string="Average Deal Size Won",
+        compute="_compute_average_deal",
+    )
