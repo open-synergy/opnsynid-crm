4e9655089f55e20bec67e84c8f682f59d913bc63
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-24 16:12:48 +0700

    [8.0.1.0.0][crm_lead_code_by_sale_team]

---
+++ crm_lead_code_by_sale_team/models/crm_case_section.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class CrmCaseSection(models.Model):
+    _inherit = "crm.case.section"
+
+    lead_sequence_id = fields.Many2one(
+        string="Leads Sequence",
+        comodel_name="ir.sequence",
+        company_dependent=True,
+    )
