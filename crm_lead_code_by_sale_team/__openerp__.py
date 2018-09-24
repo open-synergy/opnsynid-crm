2578f91a276819d61ca3ac9d4b7ffcb339670ae6
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-24 16:12:48 +0700

    [8.0.1.0.0][crm_lead_code_by_sale_team]

---
+++ crm_lead_code_by_sale_team/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "CRM Lead Code By Sale Team",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "crm_lead_code"
+    ],
+    "data": [
+        "views/crm_case_section_view.xml"
+    ],
+}
---
+++ crm_lead_code_by_sale_team/models/crm_lead.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    @api.model
+    def create(self, vals):
+        obj_crm_case_section =\
+            self.env["crm.case.section"]
+        section_id = vals.get("section_id", False)
+
+        if section_id:
+            criteria = [
+                ("id", "=", section_id)
+            ]
+            section = obj_crm_case_section.search(criteria)
+            if section.lead_sequence_id:
+                sequence = self.env["ir.sequence"].\
+                    next_by_id(
+                        section.lead_sequence_id.id)
+                vals['code'] = sequence
+        return super(CrmLead, self).create(vals)
