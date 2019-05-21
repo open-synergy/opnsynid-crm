20b639f1599463f2df420c816f64dbd7bb91537f
Andhitia Rama <andhitia.r@gmail.com>
2019-05-22 01:51:42 +0700

    [8.0.1.0.0] crm_lead_create_business_requirement_master_project

---
+++ crm_lead_create_business_requirement_master_project/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Create BR Master Project From Opportunity",
+    "version": "8.0.1.0.0",
+    "category": "Business Requirements Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "business_requirement_deliverable_crm",
+    ],
+    "data": [
+        "wizards/create_master_project.xml",
+        "views/crm_lead_views.xml",
+    ],
+}
