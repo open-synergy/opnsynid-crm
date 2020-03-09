4d54363a8e50eb541aa106fb701767cd609320cd
mikevhe18 <viriyananda.michael@gmail.com>
2020-03-09 17:31:49 +0700

    [11.0.1.0.0][crm_lead_auto_archieve]

---
+++ crm_lead_auto_archieve/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CRM Lead Day Auto Archieve",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Customer Relationship Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "depends": [
+        "crm_lead_day_on_stage",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/crm_team_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
