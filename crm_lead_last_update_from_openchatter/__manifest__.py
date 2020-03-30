d4aa5a57c7a86d8d854979c102252b0670c68895
Andhitia Rama <andhitia.r@gmail.com>
2020-03-30 23:57:29 +0700

    [11.0.1.0.0] crm_lead_last_update_from_openchatter

---
+++ crm_lead_last_update_from_openchatter/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CRM Last Update From OpenChatter",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Customer Relationship Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
