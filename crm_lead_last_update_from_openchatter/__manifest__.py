192bfe7cb550d168567237f15e3b520cf7348485
Andhitia Rama <andhitia.r@gmail.com>
2020-03-31 00:39:14 +0700

    [11.0.1.1.0] crm_lead_last_update_from_openchatter
    * Fix computation

--- crm_lead_last_update_from_openchatter/__manifest__.py
+++ crm_lead_last_update_from_openchatter/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "CRM Last Update From OpenChatter",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.1.0",
     "license": "AGPL-3",
     "category": "Customer Relationship Management",
     "website": "https://opensynergy-indonesia.com",
