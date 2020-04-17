9efec6982d7f837eddc8373e0f7ba32010529c1e
mikevhe18 <viriyananda.michael@gmail.com>
2020-04-17 15:00:13 +0700

    [11.0.1.3.0][crm_lead_workflow_policy]

---
+++ crm_lead_workflow_policy/models/crm_stage.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class CRMStage(models.Model):
+    _inherit = "crm.stage"
+
+    allowed_stage_ids = fields.Many2many(
+        string="Allowed Stages",
+        comodel_name="crm.stage",
+        relation="rel_stage_allowed_stage",
+        column1="parent_stage_id",
+        column2="stage_id",
+    )
