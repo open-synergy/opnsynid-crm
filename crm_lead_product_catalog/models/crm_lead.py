a70bf06d81a464a92a913a8f8ec13ee3dddd8242
Andhitia Rama <andhitia.r@gmail.com>
2020-12-24 23:00:31 +0700

    [crm_lead_product_catalog] 8.0.1.0.0

---
+++ crm_lead_product_catalog/models/crm_lead.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class CrmLead(models.Model):
+    _name = "crm.lead"
+    _inherit = "crm.lead"
+
+    product_ids = fields.Many2many(
+        string="Products",
+        comodel_name="product.product",
+        relation="rel_crm_lead_2_product_catalog",
+        column1="lead_id",
+        column2="product_id",
+    )
+    pricelist_id = fields.Many2one(
+        string="Pricelist",
+        comodel_name="product.pricelist",
+    )
